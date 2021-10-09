729046c8f9bb04f15458eab6ec9430009d286528
Dev4LifeV <vctudo.27@gmail.com>
2021-10-09 12:39:09 -0300

    refactor unit tests

--- test/meowButton_test.dart
+++ test/meowButton_test.dart
@@ -5,24 +5,44 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 
 void main() {
-  testWidgets('Finds a widget using a key and tap it',
-      (WidgetTester tester) async {
-    final MeowButtonController _controller = Get.put(MeowButtonController());
-    const Key meowButtonKey = Key('MeowButtonKey');
-    final keyFinder = find.byKey(meowButtonKey);
+  group('', () {
+    testWidgets('Should find a widget using a key and tap it',
+        (WidgetTester tester) async {
+      final MeowButtonController _controller = Get.put(MeowButtonController());
+      const Key meowButtonKey = Key('MeowButtonKey');
+      final keyFinder = find.byKey(meowButtonKey);
 
-    await tester.pumpWidget(Material(
-      child: Directionality(
-        child: MeowButton(
-          key: meowButtonKey,
-          onTap: _controller.fetchData,
+      await tester.pumpWidget(Material(
+        child: Directionality(
+          child: MeowButton(
+            key: meowButtonKey,
+            onTap: _controller.fetchData,
+          ),
+          textDirection: TextDirection.ltr,
         ),
-        textDirection: TextDirection.ltr,
-      ),
-    ));
+      ));
 
-    await tester.tap(keyFinder);
+      await tester.tap(keyFinder);
 
-    expect(keyFinder, findsOneWidget);
+      expect(keyFinder, findsOneWidget);
+    });
+
+    testWidgets("Shouldn't find a widget using a key",
+        (WidgetTester tester) async {
+      final MeowButtonController _controller = Get.put(MeowButtonController());
+      const Key differentKey = Key('differentKey');
+      final keyFinder = find.byKey(differentKey);
+
+      await tester.pumpWidget(Material(
+        child: Directionality(
+          child: MeowButton(
+            onTap: _controller.fetchData,
+          ),
+          textDirection: TextDirection.ltr,
+        ),
+      ));
+
+      expect(keyFinder, findsNothing);
+    });
   });
 }
